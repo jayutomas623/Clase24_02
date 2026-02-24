c0e2324269b13986905c82123167c5b113826576
Tomas <jayu062004@gmail.com>
2026-02-24 17:50:36 -0400

    CodigoCompletoDeEjemplo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,3 +2,11 @@ import {Component, signal} from '@angular/core';
 import {Routergutar} from '@angular/router';
 import {EditorFiltroComponent} from '.editor/filtro/editor-filtro';
 
+@component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [EditorFiltroComponent],
+    templateUrl: './app.html',
+    styleUrl: './app.scss'
+})
+export class App{}
